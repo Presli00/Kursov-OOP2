ca7eea63fbd26c2293767d068af7eee54c122e6e
Rumen <rumen_pavlov35@abv.bg>
2021-10-23 18:54:58 +0300

    Add functional requirements and test logo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("OWNERGUI.fxml"));
         Scene scene = new Scene(root);
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/Images/PR Warehouses.png")));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
