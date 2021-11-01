869949a266ec7d03532d8dbee1f658b299d6311a
Rumen <rumen_pavlov35@abv.bg>
2021-11-01 17:51:39 +0200

    Hibernate

--- src/main/java/AdminGUI.java
+++ src/main/java/AdminGUI.java
@@ -1,6 +1,14 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 
 public class AdminGUI {
     // Panes
@@ -10,4 +18,35 @@ public class AdminGUI {
     // Buttons
     @FXML
     Button SpravkiButton;
+
+    @FXML
+    public void SpravkiOnAction(ActionEvent event) throws IOException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        ScrollPane SpravkiScrollPane = new ScrollPane();
+        ContentAnchorPane.getChildren().add(SpravkiScrollPane);
+        SpravkiScrollPane.prefHeightProperty().bind(ContentAnchorPane.heightProperty());
+        SpravkiScrollPane.prefWidthProperty().bind(ContentAnchorPane.widthProperty());
+        SpravkiScrollPane.setFitToWidth(true);
+        SpravkiScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+        SpravkiScrollPane.setContent(ScrollAnchorPane);
+        ScrollAnchorPane.prefWidthProperty().bind(SpravkiScrollPane.widthProperty());
+        ScrollAnchorPane.prefHeightProperty().bind(SpravkiScrollPane.heightProperty());
+        SpravkiButtons obj = new SpravkiButtons();
+        obj.loadController("WarehouseAgentGUI", 5);
+        ArrayList<Button> buttons = obj.getButtons();
+        StyleButtons(buttons);
+        VBox ScrollVbox = new VBox();
+        ScrollAnchorPane.getChildren().add(ScrollVbox);
+        ScrollVbox.getChildren().addAll(buttons);
+        ScrollVbox.setSpacing(30);
+    }
+
+    public void StyleButtons(ArrayList<Button> buttons) {
+        String[] Text = {"Сключени договори", "Уредници", "Рейтинги на агенти", "Налични складове за отдаване под наем", "Складове по даден собственик"};
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).prefWidthProperty().bind(ScrollAnchorPane.widthProperty());
+            buttons.get(i).setPadding(new Insets(0));
+            buttons.get(i).setText(Text[i]);
+        }
+    }
 }
