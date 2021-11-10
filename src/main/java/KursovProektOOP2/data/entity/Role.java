8803c6b1c3c3f75db87d41e1cf2bb3adb55e10bb
Rumen <rumen_pavlov35@abv.bg>
2021-11-10 12:22:28 +0200

    entity fix

--- src/main/java/KursovProektOOP2/data/entity/Role.java
+++ src/main/java/KursovProektOOP2/data/entity/Role.java
@@ -9,9 +9,6 @@ public class Role implements Serializable {
     @Id
     @Column(name = "role_id", nullable = false)
     private int roleId;
-    @OneToOne
-    @JoinColumn(name = "Subrole_id", nullable = false)
-    private SubRole subroleId;
     @Column(name = "role_name", nullable = false)
     private String roleName;
     public int getRoleId() {
@@ -20,15 +17,6 @@ public class Role implements Serializable {
 
     public void setRoleId(int roleId) {
         this.roleId = roleId;
-    }
-
-
-    public SubRole getSubroleId() {
-        return subroleId;
-    }
-
-    public void setSubroleId(SubRole subroleId) {
-        this.subroleId = subroleId;
     }
 
     public String getRoleName() {
@@ -47,7 +35,6 @@ public class Role implements Serializable {
         Role role = (Role) o;
 
         if (roleId != role.roleId) return false;
-        if (subroleId != role.subroleId) return false;
         if (roleName != null ? !roleName.equals(role.roleName) : role.roleName != null) return false;
 
         return true;
@@ -57,7 +44,6 @@ public class Role implements Serializable {
     public String toString() {
         return "Role{" +
                 "roleId=" + roleId +
-                ", subroleId=" + subroleId +
                 ", roleName='" + roleName + '\'' +
                 '}';
     }
