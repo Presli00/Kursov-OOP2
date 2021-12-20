33b1e9d1371788fddf742024cd5ab3e1381f9898
Presli00 <ice_rock@abv.bg>
2021-12-20 21:17:18 +0200

    finished second panel in settings

--- src/main/java/KursovProektOOP2/controllers/Settings.java
+++ src/main/java/KursovProektOOP2/controllers/Settings.java
@@ -6,9 +6,13 @@ import KursovProektOOP2.util.UserSession;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import java.sql.Timestamp;
+import java.util.Date;
 
 public class Settings {
     @FXML
@@ -31,18 +35,64 @@ public class Settings {
     CheckBox passwordCheck;
     @FXML
     Label errorLabel;
+
+    @FXML
+    Text usernameText;
+    @FXML
+    Text emailText;
+    @FXML
+    Text phoneText;
+    @FXML
+    Text nameText;
+    @FXML
+    Text familyText;
+    @FXML
+    Text roleText;
+    @FXML
+    Text createdText;
+    @FXML
+    Text updatedText;
+
     private static final Logger log = Logger.getLogger(Main.class);
     int currentUser = UserSession.getUserID();
 
-    public void apply() {
+    public void initialize() {
         Session session = Connection.openSession();
         Transaction transaction = session.beginTransaction();
         String CHANGE_QUERY = "SELECT u FROM User u WHERE userId='" + currentUser + "'"; //query to change the username
 
         try {
             User result = (User) session.createQuery(CHANGE_QUERY).getSingleResult();
+            usernameText.setText(result.getUsername());
+            phoneText.setText(result.getPhone());
+            emailText.setText(result.geteMail());
+            nameText.setText(result.getFirstName());
+            familyText.setText(result.getLastName());
+            roleText.setText(result.getRoleId().getRoleName());
+            createdText.setText(result.getCreatedDate().toString());
+            if(result.getUpdatedDate()==null){
+                updatedText.setText("Has not been updated yet");
+            }
+            else {
+                updatedText.setText(result.getUpdatedDate().toString());
+            }
+        } catch (Exception ex) {
+            log.error("User not found");
+        } finally {
+            transaction.commit();
+        }
+    }
+
+    public void apply() {
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        String CHANGE_QUERY = "SELECT u FROM User u WHERE userId='" + currentUser + "'"; //query to change the username
+        Date date=new Date();
+        try {
+            User result = (User) session.createQuery(CHANGE_QUERY).getSingleResult();
             result.setUsername(usernameTF.getText());
             result.setPassword(passwordTF.getText());
+            result.setUpdatedDate(new Timestamp(date.getTime()));
             cancel();
         } catch (Exception ex) {
             log.error("Password didn't match");
@@ -78,4 +128,5 @@ public class Settings {
         conPasswordTF.clear();
         conPasswordVisibleTF.clear();
     }
+
 }
